0670a9673576ecc7e921aa4be444017a5be3759c
Jared Burrows <jaredsburrows@gmail.com>
2023-07-02 18:15:42 -0400

    recreate android, ios and web folders

--- android/app/src/main/kotlin/com/jaredsburrows/example/flutter/flutter_app_template/MainActivity.kt
+++ android/app/src/main/kotlin/com/jaredsburrows/example/flutter/flutter_app_template/MainActivity.kt
@@ -2,5 +2,5 @@ package com.jaredsburrows.example.flutter.flutter_app_template
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity() {
+class MainActivity: FlutterActivity() {
 }
