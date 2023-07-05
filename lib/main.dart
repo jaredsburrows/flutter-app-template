abe5b4da58b0de75e4e5b962243a5b0b097195c6
Jared Burrows <jaredsburrows@gmail.com>
2023-07-05 11:41:55 -0400

    add integration tests for web

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app_template/app.dart';
 import 'package:url_strategy/url_strategy.dart';
 
-import 'app.dart';
+// https://github.com/flutter/flutter/issues/116936
+bool isTesting = false;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  setPathUrlStrategy();
+
+  if (!isTesting) {
+    setPathUrlStrategy();
+  }
+
   runApp(const App());
 }
