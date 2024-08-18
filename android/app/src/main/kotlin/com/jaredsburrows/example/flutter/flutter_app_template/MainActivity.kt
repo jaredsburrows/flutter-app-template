7a76b6e14bafa846d0a99dc123eedf24b8496e8f
Jared Burrows <jaredsburrows@gmail.com>
2024-08-18 16:10:32 -0400

    update android (#186)

--- android/app/src/main/kotlin/com/jaredsburrows/example/flutter/flutter_app_template/MainActivity.kt
+++ android/app/src/main/kotlin/com/jaredsburrows/example/flutter/flutter_app_template/MainActivity.kt
@@ -2,5 +2,4 @@ package com.jaredsburrows.example.flutter.flutter_app_template
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity: FlutterActivity()
